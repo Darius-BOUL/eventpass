bb216722225938de18f5473ab2e408eccd4ba4ae
Darius BOUL <dboulanga@gmail.com>
2026-02-01 14:05:13 +0100

    Ajout du QR code automatique, footer stylisé et sécurisation du projet avec .venv

--- payments/models.py
+++ payments/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from tickets.models import Ticket
 from django.utils import timezone
-1
 
 class Payment(models.Model):
+    
     ticket = models.OneToOneField('tickets.Ticket', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, default='pending')
     timestamp = models.DateTimeField(auto_now_add=True)
-    
+
+    def __str__(self):
+        return f"Payment {self.id} - {self.status}"
 
      
 
--- tickets/models.py
+++ tickets/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.conf import settings
 from events.models import Event
-
-#import qrcode
+import qrcode  # type: ignore # <--- Décommenté
 from io import BytesIO
 from django.core.files import File
 from PIL import Image
@@ -15,12 +14,22 @@ class Ticket(models.Model):
     is_paid = models.BooleanField(default=False)
     qr_code = models.ImageField(upload_to='qrcodes/', blank=True, null=True)
 
+    # Cette méthode surveille la sauvegarde du ticket
+    def save(self, *args, **kwargs):
+        # Si payé et pas encore de QR code, on le génère
+        if self.is_paid and not self.qr_code:
+            self.generate_qr_code()
+        super().save(*args, **kwargs)
+
     def generate_qr_code(self):
         data = f"Ticket ID: {self.id}\nEvent: {self.event.title}\nUser: {self.user.username}"
-        #qr = qrcode.make(data)
+        qr = qrcode.make(data) # <--- Décommenté
+        
         buffer = BytesIO()
-        #qr.save(buffer, format='PNG')
+        qr.save(buffer, format='PNG') # <--- Décommenté
+        
         file_name = f"ticket-{self.id}.png"
+        # On enregistre le fichier dans le champ ImageField
         self.qr_code.save(file_name, File(buffer), save=False)
 
     def total_price(self) -> int:
